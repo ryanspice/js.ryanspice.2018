3a39b277524ad4a588641d67d2364fcce0a9519b
Ryan Spice <spice.ryan@hotmail.com>
2018-09-03 12:36:24 -0400

    dummy commit for last commits

--- src/modules/nav/dummy.js
+++ src/modules/nav/dummy.js
@@ -1,6 +1,10 @@
 let steve = [];
+let data = [];
 
 let count = 0;
+
+if (!window.room)
+window.room={count:0};
 
 try {
 
@@ -11,13 +15,23 @@ try {
 	count = 1;
 }
 
-for(let i = count; i>0; i--){
+try {
+
+	data = (JSON.parse(sessionStorage.getItem('saved'))) || [];
+
+}catch(e){
+
+	data = [];
+}
+
+for(let i = count; i>=0; i--){
 
 	let obj = {
 		type:`span`,
 		renderTo:'#scroll',
+		className:'cl'+JSON.stringify(data[i]),
 		style:`margin:10px;max-width:116px;height:96px;display:inline-block;`,
-		innerHTML:`<i class="menu" data-feather="map" style="margin:0px;margin-top:10px;width:100%;text-align:center;"></i><br/><h6 style="width:100%;text-align:center;	">r o o m ${i}</h6>	`,
+		innerHTML:`<i class="menu" data-feather="map" style="margin:0px;margin-top:10px;width:100%;text-align:center;"></i><br/><h6 style="width:100%;text-align:center;	">r o o m ${window.room.count++}</h6>	`,
 		onclick:(evt)=>{
 
 			console.log(evt.target.template);
@@ -26,6 +40,8 @@ for(let i = count; i>0; i--){
 
 		}
 	};
+
+
 
 	steve.push(obj);
 
