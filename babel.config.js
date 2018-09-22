f8aa54832554ca8d14f73236b3f3eec15b09a691
Ryan Spice <spice.ryan@hotmail.com>
2018-09-22 11:37:53 -0400

    cleaning up configs, removing unncessary depenendiecesi

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
-module.exports = function(api){
- api.cache(false);
-
+module.exports = function (api) {
+	api.cache(false);
 	const presets = [
 		"@babel/preset-flow",
 		[
@@ -15,27 +14,28 @@ module.exports = function(api){
 			}
 		]
 	];
-  const plugins = [
-		
-
-		["@babel/plugin-proposal-decorators", { "legacy": true }],
+	const plugins = [
+		["@babel/plugin-proposal-decorators", {
+			"legacy": true
+		}],
 		"@babel/plugin-proposal-function-sent",
 		"@babel/plugin-proposal-export-namespace-from",
+		"@babel/plugin-proposal-export-default-from",
 		"@babel/plugin-proposal-numeric-separator",
 		"@babel/plugin-proposal-throw-expressions",
-
 		"@babel/plugin-syntax-dynamic-import",
 		"@babel/plugin-syntax-import-meta",
-		["@babel/plugin-proposal-class-properties", { "loose": false }],
+		["@babel/plugin-proposal-class-properties", {
+			"loose": false
+		}],
 		"@babel/plugin-proposal-json-strings",
-
-		"@babel/plugin-transform-runtime",
+		["@babel/plugin-transform-runtime"],
 		"@babel/plugin-transform-flow-strip-types",
 		"@babel/plugin-syntax-flow"
 	];
 
-  return {
-    presets,
-    plugins
-  };
+	return {
+		presets,
+		plugins
+	};
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,15 @@
-
 /* */
-
-const filename = "ryanspice.2018.js";
-
 const webpack = require('webpack');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-//const DashboardPlugin = require('webpack-dashboard/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 /* */
+const filename = "ryanspice.2018.js";
 
 module.exports = {
-	mode:'development',
-	//mode:'production',
-
+	mode: 'development',
 	devtool: 'eval-source-maps',
-
 	entry: './src/index.js',
 	output: {
 		filename: filename,
@@ -28,47 +20,47 @@ module.exports = {
 		extensions: ['.js', '.scss', '.css'],
 		plugins: [],
 		modules: [
-		 './src',
-		 'node_modules'
+			'./src',
+			'node_modules'
 		]
 	},
 	module: {
-		rules: [
-			{
+		rules: [{
 				test: /\.js$/,
 				loader: 'babel-loader',
 				exclude: [/node_modules/]
 			},
 			{
-			  test: /bootstrap\.native/,
-			  use: {
-			    loader: 'bootstrap.native-loader'
-			  }
+				test: /bootstrap\.native/,
+				use: {
+					loader: 'bootstrap.native-loader'
+				}
 			},
 			{
-          test: /\.scss$/,
-          use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-							,options:{url:false}
-          }, {
-              loader: "sass-loader" // compiles Sass to CSS
-          }]
-      },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                use:[{loader: 'file?name=public/fonts/[name].[ext]'}]
-            }
+				test: /\.scss$/,
+				use: [{
+					loader: "style-loader" // creates style nodes from JS strings
+				}, {
+					loader: "css-loader" // translates CSS into CommonJS
+						,
+					options: {
+						url: false
+					}
+				}, {
+					loader: "sass-loader" // compiles Sass to CSS
+				}]
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				use: [{
+					loader: 'file?name=public/fonts/[name].[ext]'
+				}]
+			}
 		]
 	},
 
 	node: {
-		// prevent webpack from injecting useless setImmediate polyfill because Vue
-		// source contains it (although only uses it if it's native).
 		setImmediate: false,
-		// prevent webpack from injecting mocks to Node native modules
-		// that does not make sense for the client
 		dgram: 'empty',
 		fs: 'empty',
 		net: 'empty',
@@ -76,7 +68,7 @@ module.exports = {
 		child_process: 'empty'
 	},
 
-	plugins:[
+	plugins: [
 		new MiniCssExtractPlugin({
 
 			filename: "[name].css",
@@ -84,14 +76,16 @@ module.exports = {
 			chunkFilename: "[id].css"
 
 		}),
-		new CopyWebpackPlugin([
-	        { from: './src/index.html' },
-	        { from: './src/assets', to:'./assets/' }
-	    ]),
+		new CopyWebpackPlugin([{
+				from: './src/index.html'
+			},
+			{
+				from: './src/assets',
+				to: './assets/'
+			}
+		]),
 		new webpack.NamedModulesPlugin(),
-		new webpack.optimize.OccurrenceOrderPlugin(true),
-	//	new BundleAnalyzerPlugin()
-		//new DashboardPlugin()
+		new webpack.optimize.OccurrenceOrderPlugin(true)
 	],
 	devServer: {
 		contentBase: './dist',
